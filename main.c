54b745d7f9e9faee95aa0edbdd5e8b1025f1b09d
LuisManuelMtz99 <ray_mp90@hotmail.com>
2019-09-06 20:16:57 -0500

    practica5

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop 
 Autor:Luis Manuel Lopez Martinez
+Fecha:06/09/2019
+Grupo: 006
+Horario: 7:00am - 9:00 am
+Matricula: 1986172
 
 
 
@@ -14,15 +18,14 @@ int main(int argc, char *argv[]) {
 	int cve=0, x=0; 
 	printf("\nIngrese la nota de la materia del estudiante\n");
 	scanf("%f",&nota);
-	if(nota>=5|nota<=10){
+	if(nota>=5&&nota<=10){
 		printf("\nAPTO\n");
 		}
-		else{
-			(nota>=0|nota<5);
+		if(nota>=0&&nota<5){
 			printf("\nNo Apto\n");
 		}
-		if(nota<0|nota>10){
-			printf("ERROR");
+		if(nota<0||nota>10){
+			printf("\nERROR\n");
 		}
 		
 		
@@ -33,35 +36,61 @@ int main(int argc, char *argv[]) {
 	printf("\nActividad 2\n");
 	printf("\nIngrese un numero del 1 al 7\n");
 	scanf("%d",&x);
-	if(x=1){
+	switch(x){
+		case 1: (x=1);
 		printf("\nEl dia es Lunes\n");
-	}
-	else{
-		(x=2);
+		break;
+		case 2: (x=2);
 		printf("\nEl dia es Martes\n");
-	}
-	if(x=3){
+		break;
+		case 3: (x=3);
 		printf("\nEl dia es Miercoles\n");
-	}
-	else{
-		(x=4);
+		break;
+		case 4: (x=4);
 		printf("\nEl dia es Jueves\n");
-	}
-	if(x=5){
-	printf("\nEl dia es Viernes\n");
-}
-	else{
-	 (x=6);
-	 printf("\nEl dia es sabado\n");
-	}
-	if(x=7){
+		break;
+		case 5: (x=5);
+		printf("\nEl dia es Viernes\n");
+		break;
+		case 6: (x=6);
+		printf("\nEl dia es Sabado\n");
+		break;
+		case 7: (x=7);
 		printf("\nEl dia es Domingo\n");
-	}
-	else{
-		(x<1|x>7);
-		printf("\nERROR\n");
+		break;
+		default: printf("\nNumero no esta en el rango de 1-7\n");
+		printf("\nEl dia es Martes\n");		
 	}
 	
+	/*********************************************************************************************/
+	
+	printf("\nACTIVIDAD EXTRA\n");
+	printf("\nMenu de Restaurant\n");
+	printf("\nElija uno de los siguientes platillos\n");
+	printf("\nPizza= 1\n");
+	printf("\nHamburguesa= 2\n");
+	printf("\nPizzaDog= 3\n");
+	printf("\nSushi= 4\n");
+	printf("\nTacos= 5\n");
+	scanf("%d",&cve);
+	switch(cve){
+		case 1: (cve=1);
+		printf("\nUstes escogio pizza\n");
+		break;
+		case 2: (cve=2);
+		printf("\nUstes escogio Hamburguesa\n");
+		break;
+		case 3: (cve=3);
+		printf("\nUstes escogio PizzaDog\n");
+		break;
+		case 4: (cve=4);
+		printf("\nUstes escogio Sushi\n");
+		break;
+		case 5: (cve=5);
+		printf("\nUstes escogio Tacos\n");
+		break;
+		default: printf("\El numero no esta en el rango de 1-5\n");
+	}
 	
 	
 	return 0;
