43552985ed4585ed7cb509b4ef0a6f1d35fa0fc1
LuisManuelMtz99 <ray_mp90@hotmail.com>
2019-11-09 22:49:27 -0600

    Practica14

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 /*run this program using the console pauser or add your own getch, system("pause") or input loop 
 Autor:Luis Manuel Lopez Martinez
 Fecha:06/09/2019
@@ -9,56 +10,96 @@ Grupo: 006
 Horario: 7:00am - 9:00 am
 Matricula: 1986172
 */
+#define SIZE_NAME 20
 
-int fc(int *);
+struct pictures{
+	char name[SIZE_NAME];
+	int date_time;
 	
-int vocal( char *);
+}picture_info;
+
+struct cordinates{
+	int latitude;
+	int longitude;
+}places[4];
+
 int main(){
-	static char v[100];
-	printf("\nACTIVIDAD 1\n");
-	fflush(stdin);
-	printf("Ingrese la cadena: ");
-	gets(v);	
-	printf("El numero de vocales que tiene la cadena es: %d\n", vocal(v));
-		int A[2]={};
-		/***********************************ACTIVIDAD 2****************************************/
+ 	/*****************************************ACTIVIAD 1 ***********************************/
+ 	int i;
+ 	struct pictures pic[2];
+ 	struct pictures *r;
+ 	r=pic;
+ 	for(i=0;i<2;i++){
+ 		fflush(stdin);
+ 		printf("\nIntroduce el nombre de la fotografia: ");
+ 		gets((r+i)->name);
+ 		printf("Introduce la fecha y la hora en numeros: ");
+ 		scanf("%d",&(r+i)->date_time);
+ 		
+	 }
+	 
+	/***************************************ACTIVIDAD 2**************************************/
+	struct cordinates a,*p,*q;
+	int j,aux;
+	int A[4];
+	int B[4];
 	printf("\nACTIVIDAD 2\n");
-	printf("Dame el 1 digito: ");
-	scanf("%d",&A[1]);
-	printf("Dame el 2 digito: ");
-	scanf("%d",&A[2]);
+	places[0].latitude=200;
+	places[0].longitude=300;
+	places[1].latitude=400;
+	places[1].longitude=100;
+	places[2].latitude=100;
+	places[2].longitude=400;
+	places[3].latitude=300;
+	places[3].longitude=200;
+	p=places;
+	printf("\nOrdenado por como fueron obtenidos\n");
+	printf("\n%d %d\n%d %d\n%d %d\n%d %d\n",places[0].latitude,places[0].longitude,places[1].latitude,places[1].longitude,places[2].latitude,places[2].longitude,places[3].latitude,places[3].longitude);
 	
-	printf("\nLa suma de %d y de %d es: %d",A[1],A[2],fc(A));
-	getch();
-	return 0;
- }
-
-int vocal( char *v){
-	int c=0;
-	while(*v)
+	for(i=0;i<4;i++){
+		A[i]=(p+i)->latitude;
+	}
+	
+	for(i=1;i<4;i++)
 	{
-		
-	switch(toupper(*v)){
-		case 'A':
-       case 'E':
-       case 'I':
-       case 'O':
-       case 'U': 
-	   c++;
-	    
-     }
-     v++;
-  }
-  return c;
+		for(j=0;j<3;j++)
+		{
+		if(A[j] > A[j+1])
+		{
+			aux=A[j];
+			A[j]=A[j+1];
+			A[j+1]=aux;
+		}
+	}
 }
 
-  
-  int fc(int *A){
-  	int i,s=0, b, *c;
-  	for(i=1;i<=2;i++){
-  			s=s+A[i];
-	  }
-	  c=&s;
-	b=*c;
-	  return b;
-  }		
+printf("\nOrden creciente por latitud\n");
+for(i=0;i<4;i++){
+			printf("%d\n",A[i]);
+	
+}
+printf("\nOrdenado crecientemente por Longitud\n");
+for(i=0;i<4;i++){
+		A[i]=(p+i)->longitude;
+	}
+	
+	for(i=1;i<4;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		if(A[j] > A[j+1])
+		{
+			aux=A[j];
+			A[j]=A[j+1];
+			A[j+1]=aux;
+		}
+	}
+}
+printf("\nOrden creciente por longitud\n");
+for(i=0;i<4;i++){
+			printf("%d\n",A[i]);
+}
+	
+getch();
+return 0;
+}
